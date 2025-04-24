57ea13b9a4c11accac7880762ca53f2883594169
Julien Bouvrais <bouvrais@gmail.com>
2025-04-23 21:03:58 -0400

    Add support for Waveshare diaplays

--- src/display_manager.py
+++ src/display_manager.py
@@ -2,18 +2,24 @@ import os
 from inky.auto import auto
 from utils.image_utils import resize_image, change_orientation
 from plugins.plugin_registry import get_plugin_instance
-
+import epaper
+from PIL import Image
 
 class DisplayManager:
     def __init__(self, device_config):
         """Manages the display and rendering of images."""
         self.device_config = device_config
-        self.inky_display = auto()
-        self.inky_display.set_border(self.inky_display.BLACK)
+        
+        self.display = epaper.epaper('epd7in3f').EPD()
+        self.display.init()
+        self.display.Clear()
+
+        #self.display = auto()
+        #self.display.set_border(self.inky_display.BLACK)
 
         # store display resolution in device config
         if not device_config.get_config("resolution"):
-            device_config.update_value("resolution",[int(self.inky_display.width), int(self.inky_display.height)], write=True)
+            device_config.update_value("resolution",[int(self.display.width), int(self.display.height)], write=True)
 
     def display_image(self, image, image_settings=[]):
         """Displays the image provided, applying the image_settings."""
@@ -28,5 +34,8 @@ class DisplayManager:
         image = resize_image(image, self.device_config.get_resolution(), image_settings)
 
         # Display the image on the Inky display
-        self.inky_display.set_image(image)
-        self.inky_display.show()
+        # Create a PIL Image object from the image  
+        #pil_image = Image.fromarray(image)
+        # Display the image
+        self.display.display(self.display.getbuffer(image))
+        
